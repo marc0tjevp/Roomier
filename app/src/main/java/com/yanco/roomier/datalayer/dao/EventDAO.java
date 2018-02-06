f146d744783afa653096bf7c991b6b20f6d068b3
Unknown <ywillems96@gmail.com>
2018-02-06 17:16:18 +0100

    fixed EventDAO

--- app/src/main/java/com/yanco/roomier/datalayer/dao/EventDAO.java
+++ app/src/main/java/com/yanco/roomier/datalayer/dao/EventDAO.java
@@ -2,9 +2,41 @@ package com.yanco.roomier.datalayer.dao;
 
 import com.yanco.roomier.datalayer.dao.DAO;
 
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Marco on 06/02/2018.
  */
 
 public class EventDAO implements DAO {
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public Object getByUUID(UUID id) {
+        return null;
+    }
+
+    @Override
+    public void create(Object o) {
+
+    }
+
+    @Override
+    public void update(Object o) {
+
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+
+    }
 }
