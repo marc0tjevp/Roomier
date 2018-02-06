3b0da8591e70b8f1aa0bfa7a3ed0a0b0e6272225
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-02-06 17:20:32 +0100

    Commented out template menu structure

--- app/src/main/java/com/yanco/roomier/MainActivity.java
+++ app/src/main/java/com/yanco/roomier/MainActivity.java
@@ -80,13 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        }
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
